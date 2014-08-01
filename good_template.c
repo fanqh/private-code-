27f84145ba3e826310f14058d6d334a4408554a2
fanqh <fanqinghua@actnova.com>
2014-08-01 13:03:01 +0800

    add hex to assii

---
+++ good_template.c
@@ -0,0 +1,31 @@
+/*********************************************************************
+ * @fn      bdAddr2Str
+ *
+ * @brief   Convert Bluetooth address to string. Only needed when
+ *          LCD display is used.
+ *
+ * @return  none
+ */
+char *bdAddr2Str( uint8 *pAddr )
+{
+  uint8       i;
+  char        hex[] = "0123456789ABCDEF";
+  static char str[B_ADDR_STR_LEN];
+  char        *pStr = str;
+  
+  *pStr++ = '0';
+  *pStr++ = 'x';
+  
+  // Start from end of addr
+  pAddr += B_ADDR_LEN;
+  
+  for ( i = B_ADDR_LEN; i > 0; i-- )
+  {
+    *pStr++ = hex[*--pAddr >> 4];
+    *pStr++ = hex[*pAddr & 0x0F];
+  }
+  
+  *pStr = 0;
+  
+  return str;
+}
